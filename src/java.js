66c58c931bf535852d41e00f6f600fd1a6dc491c
LyviaAlam <154422742+LyviaAlam@users.noreply.github.com>
2024-01-04 18:14:14 +1100

    updated units and weather condition with API

--- src/java.js
+++ src/java.js
@@ -2,9 +2,41 @@ function updateNoriWeather(noriResponse) {
   let noriMainTemp = document.querySelector("#nori-main-number");
   let temperature = noriResponse.data.temperature.current;
   let cityInput = document.querySelector("#nori-main-city");
+  let noriCondition = document.querySelector("#nori-weather-condition");
+  let noriWind = document.querySelector("#nori-wind");
+  let noriHumidity = document.querySelector("#nori-humidity");
+  let noriTime = document.querySelector("#nori-time");
+  let updateDate = new Date(noriResponse.data.time * 1000);
 
   cityInput.innerHTML = noriResponse.data.city;
   noriMainTemp.innerHTML = Math.round(temperature);
+  noriCondition.innerHTML = noriResponse.data.condition.description;
+  noriWind.innerHTML = `${noriResponse.data.wind.speed}km/h`;
+  noriHumidity.innerHTML = `${noriResponse.data.temperature.humidity}%`;
+
+  noriTime.innerHTML = formatDate(updateDate);
+}
+
+function formatDate(updateDate) {
+  let hours = updateDate.getHours();
+  let minutes = updateDate.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[updateDate.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function noriSearchCity(noriCity) {
