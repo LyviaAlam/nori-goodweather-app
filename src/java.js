bdc8893e928b1691949a32990c8cdb7f375b1c73
LyviaAlam <154422742+LyviaAlam@users.noreply.github.com>
2024-01-04 17:17:49 +1100

    Added Axios and Weather API to search engine

--- src/java.js
+++ src/java.js
@@ -1,9 +1,26 @@
+function updateNoriWeather(noriResponse) {
+  let noriMainTemp = document.querySelector("#nori-main-number");
+  let temperature = noriResponse.data.temperature.current;
+  let cityInput = document.querySelector("#nori-main-city");
+
+  cityInput.innerHTML = noriResponse.data.city;
+  noriMainTemp.innerHTML = Math.round(temperature);
+}
+
+function noriSearchCity(noriCity) {
+  let apiKey = "143af7fd5b08cab06a8bf5bo4f3btde9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${noriCity}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateNoriWeather);
+}
+
 function noriHandleSearch(event) {
   event.preventDefault();
   let noriInput = document.querySelector("#nori-search-form");
-  let cityInput = document.querySelector("#nori-main-city");
-  cityInput.innerHTML = noriInput.value;
+
+  noriSearchCity(noriInput.value);
 }
 
 let noriSearch = document.querySelector("#nori-search");
 noriSearch.addEventListener("submit", noriHandleSearch);
+
+noriSearchCity("Sydney");
